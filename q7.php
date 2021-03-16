091e044b6a32a8a1ed32f79eafc1d93a044b8ec6
TheusHcod <51010789+TheusHcod@users.noreply.github.com>
2021-03-16 12:59:47 -0300

    Update q7.php

--- q7.php
+++ q7.php
@@ -1,5 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="exercicio.php" method="POST">
+        <input type="text" name="numForm[0]" id="">
+        <input type="text" name="numForm[1]" id="">
+        <input type="text" name="numForm[2]" id="">
+        <input type="text" name="numForm[3]" id="">
+        <input type="text" name="numForm[4]" id="">
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
+
+
 <?php
 
-
-
-printf (max(array('1','5','13','22','98')));
+    $numForm = $_POST['numForm'];
+    printf (max($numForm));
